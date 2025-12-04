af2b1df436c6c8fde23ab763f852645aa181f024
GitHub Action <action@github.com>
2025-12-04 07:04:20 +0000

    🎨 Auto-format code with pre-commit

--- dingo/model/rule/base.py
+++ dingo/model/rule/base.py
@@ -9,7 +9,7 @@ class BaseRule:
     metric_type: str  # This will be set by the decorator
     group: List[str]  # This will be set by the decorator
     dynamic_config: EvaluatorRuleArgs
-    
+
     # Quality label constants
     LABEL_QUALITY_GOOD = "QUALITY_GOOD"  # Indicates pass the quality check
     LABEL_QUALITY_BAD_PREFIX = "QUALITY_BAD_"  # Indicates not pass the quality check
@@ -17,13 +17,13 @@ class BaseRule:
     @classmethod
     def eval(cls, input_data: Data) -> ModelRes:
         """评估输入数据的质量
-        
+
         子类应该覆盖此方法实现具体的评估逻辑。
-        
+
         标准实现模式：
         ```python
         res = ModelRes()
-        
+
         # 检查是否存在质量问题
         if 检测到问题:
             res.eval_status = True  # True 表示发现问题
@@ -36,13 +36,13 @@ class BaseRule:
             res.eval_details = {
                 "label": [cls.LABEL_QUALITY_GOOD]
             }
-        
+
         return res
         ```
-        
+
         Args:
             input_data: 待评估的数据对象
-            
+
         Returns:
             ModelRes: 评估结果对象
         """

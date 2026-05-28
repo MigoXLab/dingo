64ba8a8fce8e31560783f630f1f64cfe3028da05
chupei <njuchupei@gmail.com>
2026-05-28 11:22:27 +0800

    feat: unify image loader

--- dingo/data/__init__.py
+++ dingo/data/__init__.py
@@ -1,4 +1,3 @@
 from dingo.data.converter import BaseConverter, converters  # noqa E402.
 from dingo.data.dataset import Dataset, dataset_map  # noqa E402.
 from dingo.data.datasource import DataSource, datasource_map  # noqa E402.
-from dingo.data.searcher import Searcher, SearchResult, searcher_map  # noqa E402.

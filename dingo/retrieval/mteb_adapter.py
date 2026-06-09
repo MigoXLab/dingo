ec3e2fde699e1f4822774938c027d15e99f470b7
chupei <njuchupei@gmail.com>
2026-06-09 14:44:45 +0800

    x

--- dingo/retrieval/mteb_adapter.py
+++ dingo/retrieval/mteb_adapter.py
@@ -70,6 +70,8 @@ def _build_effective_query_text(
 ) -> str:
     if not _task_uses_query_instructions(task_name) or not instruction:
         return query_text
+    if query_text and query_text in instruction:
+        return instruction
     return f"{query_text} {instruction}"
 
 

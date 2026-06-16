4902aa6733a533f0cb59bfa331db3f64429bd2a4
RheagalFire <arishalam121@gmail.com>
2026-06-16 22:50:28 +0530

    chore: tighten litellm pin to >=1.80.0,<1.87.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requirements = _read_requirements("./requirements/runtime.txt")
 
 agent_requirements = _read_requirements("./requirements/agent.txt")
 hhem_requirements = _read_requirements("./requirements/hhem_integration.txt")
-litellm_requirements = ["litellm>=1.35.0,<2.0"]
+litellm_requirements = ["litellm>=1.80.0,<1.87.0"]
 
 extras_require = {
     'agent': agent_requirements,
